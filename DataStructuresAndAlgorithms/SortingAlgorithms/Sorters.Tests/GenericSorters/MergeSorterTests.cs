1ace4585d4c31a422024d7dfbda82b50a20ab899
pspassova <petya.spassova@abv.bg>
2017-01-04 20:08:10 +0200

    Changed name

---
+++ DataStructuresAndAlgorithms/SortingAlgorithms/Sorters.Tests/GenericSorters/MergeSorterTests.cs
@@ -0,0 +1,108 @@
+﻿using Sorters.GenericSorters;
+
+using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace Sorters.Tests.GenericSorters
+{
+    [TestFixture]
+    public class MergeSorterTests
+    {
+        [Test]
+        public void SortMethodShould_SortElementsProperly_IfTheyAreOfTypeFloat()
+        {
+            // Arrange 
+            IList<float> floatElements = new List<float> { 10.2f, -14f, 8.46f, 12f, 1f, -18f, 6f };
+            IList<float> sortedFloatElements = new List<float> { -18f, -14f, 1f, 6f, 8.46f, 10.2f, 12 };
+
+            QuickSorter<float> sorter = new QuickSorter<float>();
+
+            // Act
+            IList<float> actualSortedElements = sorter.Sort(floatElements);
+
+            // Assert
+            Assert.AreEqual(sortedFloatElements, actualSortedElements);
+        }
+
+        [Test]
+        public void SortMethodShould_SortElementsProperly_IfTheyAreOfTypeDecimal()
+        {
+            // Arrange 
+            IList<decimal> decimalElements = new List<decimal> { 10.2m, 8.46m, 12m, -14m, 1m, -18m, 6m };
+            IList<decimal> sortedDecimalElements = new List<decimal> { -18m, -14m, 1m, 6m, 8.46m, 10.2m, 12m };
+
+            QuickSorter<decimal> sorter = new QuickSorter<decimal>();
+
+            // Act
+            IList<decimal> actualSortedElements = sorter.Sort(decimalElements);
+
+            // Assert
+            Assert.AreEqual(sortedDecimalElements, actualSortedElements);
+        }
+
+        [Test]
+        public void SortMethodShould_SortElementsProperly_IfTheyAreOfTypeDouble()
+        {
+            // Arrange 
+            IList<double> doubleElements = new List<double> { 10.2d, 8.46d, 12d, -14d, 1d, -18d, 6d };
+            IList<double> sortedDoubleElements = new List<double> { -18d, -14d, 1d, 6d, 8.46d, 10.2d, 12d };
+
+            QuickSorter<double> sorter = new QuickSorter<double>();
+
+            // Act
+            IList<double> actualSortedElements = sorter.Sort(doubleElements);
+
+            // Assert
+            Assert.AreEqual(sortedDoubleElements, actualSortedElements);
+        }
+
+        [Test]
+        public void SortMethodShould_SortElementsProperly_IfTheyAreOfTypeInteger()
+        {
+            // Arrange 
+            IList<int> integerElements = new List<int> { 10, 2, 8, 4, -6, 12, -20, 14, -18 };
+            IList<int> sortedIntegerElements = new List<int> { -20, -18, -6, 2, 4, 8, 10, 12, 14 };
+
+            QuickSorter<int> sorter = new QuickSorter<int>();
+
+            // Act
+            IList<int> actualSortedElements = sorter.Sort(integerElements);
+
+            // Assert
+            Assert.AreEqual(sortedIntegerElements, actualSortedElements);
+        }
+
+        [Test]
+        public void SortMethodShould_SortElementsProperly_IfTheyAreOfTypeChar()
+        {
+            // Arrange 
+            IList<char> charElements = new List<char> { 'b', 'a', 'c', 'f', 'e' };
+            IList<char> sortedCharElements = new List<char> { 'a', 'b', 'c', 'e', 'f' };
+
+            QuickSorter<char> sorter = new QuickSorter<char>();
+
+            // Act
+            IList<char> actualSortedElements = sorter.Sort(charElements);
+
+            // Assert
+            Assert.AreEqual(sortedCharElements, actualSortedElements);
+        }
+
+        [Test]
+        public void SortMethodShould_SortElementsProperly_IfTheyAreOfTypeString()
+        {
+            // Arrange 
+            IList<string> stringElements = new List<string> { "gg", "ga", "p", "v", "c" };
+            IList<string> sortedStringElements = new List<string> { "c", "ga", "gg", "p", "v" };
+
+            QuickSorter<string> sorter = new QuickSorter<string>();
+
+            // Act
+            IList<string> actualSortedElements = sorter.Sort(stringElements);
+
+            // Assert
+            Assert.AreEqual(sortedStringElements, actualSortedElements);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/SortingAlgorithms/Sorters/GenericSorters/MergeSorter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sorters.GenericSorters
+{
+    public class MergeSorter<T>
+        where T : IComparable<T>
+    {
+        public IList<T> Sort(List<T> elements)
+        {
+            var sortedElements = this.SortHelper(elements);
+
+            elements.Clear();
+            elements.AddRange(sortedElements);
+
+            return elements;
+        }
+
+        private List<T> SortHelper(IList<T> elements)
+        {
+            int elementsCount = elements.Count / 2;
+            if (elementsCount < 2)
+            {
+                return elements.ToList();
+            }
+
+            List<T> leftPart = new List<T>();
+            List<T> rightPart = new List<T>();
+
+            leftPart.AddRange(elements.Take(elementsCount));
+            rightPart.AddRange(elements.Skip(elementsCount));
+
+            leftPart = this.SortHelper(leftPart);
+            rightPart = this.SortHelper(rightPart);
+
+            return this.Merge(leftPart, rightPart);
+        }
+
+        private List<T> Merge(IList<T> leftPart, IList<T> rightPart)
+        {
+            var elements = new List<T>(leftPart.Count + rightPart.Count);
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+            for (int i = 0; i < elements.Capacity; i++)
+            {
+                if (leftIndex >= leftPart.Count)
+                {
+                    elements.Add(rightPart[rightIndex]);
+                    rightIndex++;
+                }
+                else if (rightIndex >= rightPart.Count)
+                {
+                    elements.Add(leftPart[leftIndex]);
+                    leftIndex++;
+                }
+                else if (leftPart[leftIndex].CompareTo(rightPart[rightIndex]) > 0)
+                {
+                    elements.Add(rightPart[rightIndex]);
+                    rightIndex++;
+                }
+                else
+                {
+                    elements.Add(leftPart[leftIndex]);
+                    leftIndex++;
+                }
+            }
+
+            return elements;
+        }
+    }
+}
