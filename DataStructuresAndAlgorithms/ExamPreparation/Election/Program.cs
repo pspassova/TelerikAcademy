f614a69f2ae93d3d115004d1e75434fdf6061746
pspassova <petya.spassova@abv.bg>
2017-01-04 20:08:41 +0200

    Dynamic Programming solution

---
+++ DataStructuresAndAlgorithms/ExamPreparation/Election/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Election
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int seats = int.Parse(Console.ReadLine());
+            int partiesCount = int.Parse(Console.ReadLine());
+
+            int[] partiesSeats = new int[partiesCount];
+            for (int i = 0; i < partiesCount; i++)
+            {
+                partiesSeats[i] = int.Parse(Console.ReadLine());
+            }
+
+            BigInteger possibleCombinations = FindCombinations(partiesSeats, seats);
+
+            Console.WriteLine(possibleCombinations);
+        }
+
+        private static BigInteger FindCombinations(int[] partiesSeats, int seats)
+        {
+            BigInteger[] combinations = new BigInteger[partiesSeats.Sum() + 1];
+
+            combinations[0] = 1; // base
+
+            foreach (int partySeats in partiesSeats)
+            {
+                for (int i = combinations.Length - 1; i >= 0; i--)
+                {
+                    if (combinations[i] > 0)
+                    {
+                        combinations[i + partySeats] += combinations[i];
+                    }
+                }
+            }
+
+            BigInteger possibleCombinations = new BigInteger(0);
+            for (int i = seats; i < combinations.Length; i++)
+            {
+                possibleCombinations += combinations[i];
+            }
+
+            return possibleCombinations;
+        }
+    }
+}
