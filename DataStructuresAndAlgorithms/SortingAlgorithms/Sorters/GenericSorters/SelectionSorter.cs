339606a38359bc9709211df9fbec222738fcce5c
pspassova <petya.spassova@abv.bg>
2017-01-04 20:26:58 +0200

    Changed name

---
+++ DataStructuresAndAlgorithms/SortingAlgorithms/Sorters/GenericSorters/SelectionSorter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorters.GenericSorters
+{
+    public class SelectionSorter<T>
+        where T : IComparable<T>
+    {
+        public IList<T> Sort(IList<T> elements)
+        {
+            for (int i = 0; i < elements.Count - 1; i++)
+            {
+                int currentMin = i;
+                for (int j = i + 1; j < elements.Count; j++)
+                {
+                    if (elements[currentMin].CompareTo(elements[j]) > 0)
+                    {
+                        currentMin = j;
+                    }
+                }
+
+                T temp = elements[i];
+                elements[i] = elements[currentMin];
+                elements[currentMin] = temp;
+            }
+
+            return elements;
+        }
+    }
+}
