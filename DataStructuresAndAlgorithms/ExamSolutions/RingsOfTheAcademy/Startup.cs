e339665b2d7a4a4192d1dc26d31b3da8d5a8fc84
pspassova <petya.spassova@abv.bg>
2017-01-11 18:00:41 +0200

    Rings of the Academy - Solution

---
+++ DataStructuresAndAlgorithms/ExamSolutions/RingsOfTheAcademy/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace RingsOfTheAcademy
+{
+    // Test:
+    // 6
+    // 0
+    // 1
+    // 2
+    // 3
+    // 4
+    // 4
+    public class Startup
+    {
+        public static void Main()
+        {
+            int ringsCount = int.Parse(Console.ReadLine());
+
+            int[] rings = new int[ringsCount + 1];
+            for (int i = 0; i < ringsCount; i++)
+            {
+                int parentRing = int.Parse(Console.ReadLine());
+                rings[parentRing]++;
+            }
+
+            BigInteger[] factorials = CalculateFactorials(rings);
+
+            BigInteger ringConfigurations = 1;
+            for (int i = 1; i < rings.Length; i++)
+            {
+                ringConfigurations *= factorials[rings[i]];
+            }
+
+            Console.WriteLine(ringConfigurations);
+        }
+
+        private static BigInteger[] CalculateFactorials(int[] numbers)
+        {
+            BigInteger[] factorials = new BigInteger[numbers.Max() + 1];
+            factorials[0] = 1;
+
+            for (int i = 1; i < factorials.Length; i++)
+            {
+                factorials[i] = factorials[i - 1] * i;
+            }
+
+            return factorials;
+        }
+    }
+}
