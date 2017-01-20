14f273bf6a922d6c2628f6f2deb4579b94bb2994
pspassova <petya.spassova@abv.bg>
2017-01-21 01:00:49 +0200

    Simple MVC sumator added

---
+++ WebForms/01. Introduction-to-ASP.NET/SumatorMVC/SumatorMVC/Controllers/SumatorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace SumatorMVC.Controllers
+{
+    public class SumatorController : Controller
+    {
+        public ActionResult Sumator(double? sum)
+        {
+            this.ViewBag.Sum = sum;
+            return this.View();
+        }
+
+        public ActionResult Sum(double? firstNumber, double? secondNumber)
+        {
+            return this.RedirectToAction("Sumator", new
+            {
+                sum = firstNumber + secondNumber
+            });
+        }
+    }
+}
---
+++ WebForms/01. Introduction-to-ASP.NET/SumatorMVC/SumatorMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SumatorMVC.Startup))]
+namespace SumatorMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
