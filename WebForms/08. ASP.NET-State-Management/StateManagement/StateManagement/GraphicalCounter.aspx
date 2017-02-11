2dc402f48d0e660662eb002f54e9e9f34ff8c04c
pspassova <petya.spassova@abv.bg>
2017-02-11 13:20:49 +0200

    Draw image from string task added

--- WebForms/08. ASP.NET-State-Management/StateManagement/StateManagement/GraphicalCounter.aspx
+++ WebForms/08. ASP.NET-State-Management/StateManagement/StateManagement/GraphicalCounter.aspx
@@ -1,3 +1,5 @@
 ﻿<%@ Page Title="Graphical Counter" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GraphicalCounter.aspx.cs" Inherits="StateManagement.GraphicalCounter" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="UsersCountLabel" runat="server"></asp:Label>
 </asp:Content>
--- WebForms/08. ASP.NET-State-Management/StateManagement/StateManagement/GraphicalCounter.aspx.cs
+++ WebForms/08. ASP.NET-State-Management/StateManagement/StateManagement/GraphicalCounter.aspx.cs
@@ -1,17 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace StateManagement
 {
-    public partial class GraphicalCounter : System.Web.UI.Page
+    public partial class GraphicalCounter : Page
     {
+        protected override void OnPreLoad(EventArgs e)
+        {
+            int visitorsCount = 0;
+            if (this.Session["visits"] == null)
+            {
+                this.Session["visits"] = 1;
+            }
+            else
+            {
+                visitorsCount = (int)this.Session["visits"];
+
+                this.Session["visits"] = visitorsCount++;
+            }
+
+            DrawImageFromVisitorsCount(visitorsCount);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        private void DrawImageFromVisitorsCount(int visitorsCount)
+        {
+            Bitmap image = new Bitmap(400, 200);
+            using (image)
+            {
+                Graphics graphics = Graphics.FromImage(image);
+                using (graphics)
+                {
+                    graphics.DrawString($"Visitors count: {visitorsCount}",
+                        new Font("Segoe UI", 25),
+                        new SolidBrush(Color.Bisque),
+                        new PointF(100, 100));
+
+                    this.Response.ContentType = "image/jpeg";
+                    image.Save(this.Response.OutputStream, ImageFormat.Jpeg);
+                }
+            }
         }
     }
 }
