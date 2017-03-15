9ae9465aea3f451f068a56050bac02ccc8a9bdfa
Petya <spassovap@gmail.com>
2017-03-15 19:56:14 +0200

    Fixed readme

---
+++ MVC/01. Introduction-to-ASP.NET-MVC/Calculator/Calculator/Models/DataViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Calculator.Models.Enumerations;
+
+namespace Calculator.Models
+{
+    public class DataViewModel
+    {
+        public UnitType Type { get; set; }
+
+        public double Value { get; set; }
+    }
+}
